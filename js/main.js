0b0eb8a2f6830d14da809609ece285005affe3b3
Wouter van Baalen <302605475@student.rocmondriaan.nl>
2021-04-22 14:53:18 +0200

    de nieuwste geüpload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const button = document.querySelector(".theme-button");
+let lightModeOn = JSON.parse(localStorage.getItem("lightModeOn"));
+if (~lightModeOn){
+    button.textContent = "light mode"
+}
+const changeHandeler = function (event) {
+    if (lightModeOn) {
+        lightModeOn = false;
+        const body=document.querySelector("body")
+        body.classList.add("dark-mode")
+        button.textContent ="light mode" 
+    }   else {
+        lightModeOn = true;
+        const body=document.querySelector("body")
+        body.classList.remove("dark-mode")
+        button.textContent = "dark mode"
+    }
+    localStorage.setItem("lightModeOn", lightModeOn);
+}
+button.addEventListener("click",changeHandeler);
+
+if (lightModeOn === false) {
+    const body=document.querySelector("body")
+    body.classList.add("dark-mode")  
+}
